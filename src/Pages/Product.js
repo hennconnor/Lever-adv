884b21633195affbbb21c931729496968f729b2f
Connor Henn <connor.henn@yahoo.com>
2024-07-08 07:59:00 -0700

    added pricing

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -9,10 +9,18 @@ const Product = ({ products }) => {
             <div className='text-black p-5 bg-white w-5/6 mx-auto rounded-md border-solid border-blue-200 border-2 min-w-80'>
                 <h1 className='underline mb-2 font-bold text-lg'>{products[current].name}</h1>
                 <img src={products[current].image} alt={products[current].alt} className='border-2 mb-4' />
-                <h2 className='font-bold border-b-2 my-2 pb-2'>Highlights</h2>
-                <ul className='list-disc my-3 ml-4'>
-                    {products[current].details.map((element) => <li className='my-2 leading-8'>{element}</li>)}
-                </ul>
+                <div>
+                    <h2 className='font-bold border-b-2 my-2 pb-2'>Highlights</h2>
+                    <ul className='list-disc my-3 ml-4'>
+                        {products[current].details.map((element) => <li className='my-2 leading-8'>{element}</li>)}
+                    </ul>
+                </div>
+                <div>
+                    <h2 className='font-bold border-b-2 my-2 pb-2'>Pricing</h2>
+                    <ul className='list-disc my-3 ml-4'>
+                        {products[current].pricing.map((element) => <li className='my-2 leading-8'>{element}</li>)}
+                    </ul>
+                </div>
                 <div className='flex items-center justify-center gap-3'>
                     <button className='border-2 p-2 border-red-400 text-red-400 hover:bg-red-400 hover:text-white'>Order Now</button>
                     <button className='border-2 p-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white'>Order Sample</button>
--- src/Products.js
+++ src/Products.js
@@ -11,41 +11,47 @@ export const products = [
         name: "Montreux Pen",
         image: img1,
         alt: "Montreux Pen with customized imprint",
-        details: ['Your order will contain 7 sparkling, translucent barrel colors', 'You may order as few as 100 pieces.', 'We print your message in 3 locations, all of which are always in view!', 'Ink refill color is dark blue', 'Generous ink refill writes 1.5 miles!  Your clients are unlikely to ever run out of ink.']
+        details: ['Your order will contain 7 sparkling, translucent barrel colors', 'You may order as few as 100 pieces.', 'We print your message in 3 locations, all of which are always in view!', 'Ink refill color is dark blue', 'Generous ink refill writes 1.5 miles!  Your clients are unlikely to ever run out of ink.'],
+        pricing: ['100 quantity - $0.95/per item', '250 quantity - $0.87/per item', '500 quantity - $0.79/per item', '1,000 quantity - $0.75/per item']
     },
     {
         id: 2,
         name: "Cinema 500 Pen",
         image: img2,
         alt: "Cinema 500 Pen with customized imprint",
-        details: ['Your price includes imprints on the top of barrel and clip – always in view!', 'Generous ink refill writes 1.5 miles!  Your clients are unlikely to ever run out of ink.', 'Order our assortment of 6 vibrant colors, or pick any individual color! (250 minimum per color)', 'Ink refill color is dark blue']
+        details: ['Your price includes imprints on the top of barrel and clip – always in view!', 'Generous ink refill writes 1.5 miles!  Your clients are unlikely to ever run out of ink.', 'Order our assortment of 6 vibrant colors, or pick any individual color! (250 minimum per color)', 'Ink refill color is dark blue'],
+        pricing: ['500 quantity - $0.59/per item', '1000 quantity - $0.52/per item']
     },
     {
         id: 3,
         name: "Genesis Pen",
         image: img3,
         alt: "Genesis Pen with customized imprint",
-        details: ['The barrel is made from 100% recycled product, including discarded PET plastic bottles.', 'Your purchase of this pen helps protect our oceans from the flood of plastic waste.', 'Extra large ink refill delivers up to 3.5 miles of write out – a lifetime supply for most users.', 'We print your message in 3 locations, all of which are always in view.', 'Small minimum order – just 100 pieces']
+        details: ['The barrel is made from 100% recycled product, including discarded PET plastic bottles.', 'Your purchase of this pen helps protect our oceans from the flood of plastic waste.', 'Extra large ink refill delivers up to 3.5 miles of write out – a lifetime supply for most users.', 'We print your message in 3 locations, all of which are always in view.', 'Small minimum order – just 100 pieces'],
+        pricing: ['100 quantity - $1.35/per item', '250 quantity - $1.19/per item', '500 quantity - $1.09/per item', '1000 quantity - $1.05/per item']
     },
     {
         id: 4,
         name: "Easy Flow Pen",
         image: img4,
         alt: "Easy Flow Pen with customized imprint",
-        details: ['The easiest writing ballpoint pen ever.  Flows effortlessly!)', 'Generous 1,200 meter ink refill supply', 'Ships in 4 sparkling, translucent barrel colors', '3 imprint areas located where your message is always in sight!']
+        details: ['The easiest writing ballpoint pen ever.  Flows effortlessly!)', 'Generous 1,200 meter ink refill supply', 'Ships in 4 sparkling, translucent barrel colors', '3 imprint areas located where your message is always in sight!'],
+        pricing: ['300 quantity - $0.95/per item', '600 quantity - $0.91/per item', '1,200 quantity - $0.87/per item']
     },
     {
         id: 5,
         name: "Triple Beam Flatlight",
         image: img5,
         alt: "Triple Beam Flashlight with customized imprint",
-        details: ['Long lithium battery life – up to 3 years and batteries replaceable', '3 powerful LED beams throw a generous amount of light to show the way', 'Flat shape can fit in most tight spaces and travels well.', 'Large imprint area to generously highlight your message']
+        details: ['Long lithium battery life – up to 3 years and batteries replaceable', '3 powerful LED beams throw a generous amount of light to show the way', 'Flat shape can fit in most tight spaces and travels well.', 'Large imprint area to generously highlight your message'],
+        pricing: []
     },
     {
         id: 6,
         name: "Powerstroke Letter Opener",
         image: img6,
         alt: "Powerstroke Letter Opener with customized imprint",
-        details: ['MORE POWER -The long gripping area gives you a powerful base from which to push the blade forward, so you quite naturally apply more force to the task.', 'FAR SAFER TO USE - Our design keeps your hands far away from the cutting process, preventing accidental nicks or those nasty paper cuts!', 'GENEROUS IMPRINT AREA -Your company’s message can contain up to 4 lines of text, or your logo in up to 2 colors.   Your clients will visually connect with you every time they open their mail!']
+        details: ['MORE POWER -The long gripping area gives you a powerful base from which to push the blade forward, so you quite naturally apply more force to the task.', 'FAR SAFER TO USE - Our design keeps your hands far away from the cutting process, preventing accidental nicks or those nasty paper cuts!', 'GENEROUS IMPRINT AREA -Your company’s message can contain up to 4 lines of text, or your logo in up to 2 colors.   Your clients will visually connect with you every time they open their mail!'],
+        pricing: ['as low as $1.29 each, price dependent on quantity']
     }
 ]
