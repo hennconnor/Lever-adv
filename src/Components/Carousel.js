36323350b27339b75af1e26749346d07d69f4dfb
Connor Henn <connor.henn@yahoo.com>
2024-09-12 08:06:16 -0700

    added arrow overlay to carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -25,21 +25,23 @@ const Carousel = () => {
         setSlide(slide === products.length - 1 ? 0 : slide + 1);
     }
 
-    const circleClick = (index) => {
-        setSlide(index)
-    }
+    // const circleClick = (index) => {
+    //     setSlide(index)
+    // }
     return (
         <div className='my-8'>
-            <div>
+            <div className='relative'>
                 {products.map((product, index) => { return <CarouselItem product={product} key={index} slide={slide} index={index} /> })}
+                <button onClick={clickLeft}><FaArrowLeft className='cursor-pointer md:size-9 size-6 bg-slate-600 opacity-70 hover:opacity-100 hover:scale-125 duration-500 absolute top-1/2 left-2' /></button>
+                <button onClick={clickRight}><FaArrowRight className='cursor-pointer bg-slate-600 size-6 md:size-9 opacity-70 hover:opacity-100 hover:scale-125 duration-500 absolute top-1/2 right-2' /></button>
             </div>
-            <div className='flex justify-around items-center bg-[#0d364f] p-2 border-stone-400 border-solid border-t-2'>
+            {/* <div className='flex justify-around items-center bg-[#0d364f] p-2 border-stone-400 border-solid border-t-2'>
                 <button onClick={clickLeft}><FaArrowLeft className='cursor-pointer size-7 opacity-80 hover:opacity-100 hover:scale-125 duration-500' /></button>
                 {products.map((product, index) => {
                     return <div onClick={() => circleClick(index)}>{slide === index ? <button><FaCircleDot className='size-5 scale-125' /></button> : <button><FaCircle className='cursor-pointer opacity-80 size-5 hover:opacity-100 hover:scale-125 duration-500' /></button>}</div>
                 })}
                 <button onClick={clickRight}><FaArrowRight className='cursor-pointer size-7 opacity-80 hover:opacity-100 hover:scale-125 duration-500' /></button>
-            </div>
+            </div> */}
         </div>
     )
 }
